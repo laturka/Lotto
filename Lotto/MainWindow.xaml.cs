1c699f8ed86861598487858aef9babe118e03f96
Cem Akgül <cemakgul@live.de>
2019-11-14 11:30:19 +0100

    auskommentieren von TextBoxen

--- Lotto/MainWindow.xaml.cs
+++ Lotto/MainWindow.xaml.cs
@@ -10,12 +10,11 @@ namespace Lotto
     public partial class MainWindow : Window
     {
 
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            textBox1.Clear();
-            textBox2.Clear();
+            //textBox1.Clear();
+            //textBox2.Clear();
 
             Random rnd = new Random();
             List<int> myList = new List<int>();
@@ -48,9 +47,67 @@ namespace Lotto
 
         }
 
-        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        
+        private void Button_Clickeuro(object sender, RoutedEventArgs e)
         {
+            textBox3.Clear();
+            textBox4.Clear();
 
+            Random rnd = new Random();
+            List<int> euroList = new List<int>();
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                int eurozahlen = rnd.Next(1, 51);
+
+                if (!euroList.Contains(eurozahlen))
+                {
+                    euroList.Add(eurozahlen);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            euroList.Sort();
+
+            foreach (int eurozahlen in euroList)
+            {
+                string c = eurozahlen.ToString();
+                textBox3.Text += c + " ";
+            }
+
+            List<int> eurosuperzahlList = new List<int>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                int eurosuperzahl = rnd.Next(1, 11);
+                if (!eurosuperzahlList.Contains(eurosuperzahl))
+                {
+                    eurosuperzahlList.Add(eurosuperzahl);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            eurosuperzahlList.Sort();
+
+            foreach (int eurosuperzahl in eurosuperzahlList)
+            {
+                string c = eurosuperzahl.ToString();
+                textBox4.Text += c + " ";
+            }
+            
+        }
+        private void Button_Click_aufraumen(object sender, RoutedEventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
         }
     }
 }
